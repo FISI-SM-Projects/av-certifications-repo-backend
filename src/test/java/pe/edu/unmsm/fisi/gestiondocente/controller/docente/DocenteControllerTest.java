87e16fbd18022d11c6e35ef76b41e1ddb22959e8
Erick-Alegre <erickalegrem09@gmail.com>
2026-08-07 16:45:56 -0500

    aplication.properties

--- src/test/java/pe/edu/unmsm/fisi/gestiondocente/controller/docente/DocenteControllerTest.java
+++ src/test/java/pe/edu/unmsm/fisi/gestiondocente/controller/docente/DocenteControllerTest.java
@@ -19,9 +19,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
-
 import pe.edu.unmsm.fisi.gestiondocente.service.constancia.ConstanciaQueryService;
 
 @SpringBootTest
@@ -31,7 +30,7 @@ class DocenteControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
+    @MockitoBean
     private ConstanciaQueryService constanciaQueryService;
 
     @BeforeEach
