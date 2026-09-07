4f7ce9e4a1690872036420c118261c08d0a4272a
Richard Maycol Torres Rivera <richard.torres4@unmsm.edu.pe>
2026-09-07 12:06:48 -0500

    refactor: del demo auth

--- src/main/java/pe/edu/unmsm/fisi/gestiondocente/auth/exception/DemoUserNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package pe.edu.unmsm.fisi.gestiondocente.auth.exception;
-
-public class DemoUserNotFoundException extends RuntimeException {
-
-    public DemoUserNotFoundException(String message) {
-        super(message);
-    }
-}
